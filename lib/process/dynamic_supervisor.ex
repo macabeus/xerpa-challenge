02513ce93be201926fdb9907b3becf34bb914347
macabeus <bruno.macabeus@gmail.com>
2019-02-01 12:07:42 -0200

    process: add dynamic supervisor

---
+++ lib/process/dynamic_supervisor.ex
@@ -0,0 +1,11 @@
+defmodule Xerpa.DynamicSupervisor do
+  use DynamicSupervisor
+
+  def start_link() do
+    DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+end
